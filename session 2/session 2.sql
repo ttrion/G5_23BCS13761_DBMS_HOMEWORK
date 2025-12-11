bb48f686080dabe1113b21c7e7f336ad64260104
ttrion <23bcs13761@cuchd.in>
2025-12-11 11:28:07 +0530

    Add files via upload

---
+++ session 2/session 2.sql
@@ -0,0 +1,11 @@
+-- G1_23BCS13761_Tanay Manish Nesari_session2 --
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      SELECT DISTINCT(salary) from Employee order by salary DESC
+      LIMIT 1 OFFSET N
+      
+  );
+END
